4639e3ada3e1aeb44c61aab008b8af7508b74ff0
seongjaee <labyrinthird@gmail.com>
2023-06-12 17:15:38 +0900

    feat: 상영일정 모달 컴포넌트 추가

--- app/routes/calendar/index.tsx
+++ app/routes/calendar/index.tsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import { Form } from "@remix-run/react";
+import React from "react";
+import CalendarModal from "~/components/calendar/CalendarModal";
 
-type Props = {}
+type Props = {};
 
 const Calendar = (props: Props) => {
   return (
-    <div>index</div>
-  )
-}
+    <div>
+      index
+      <Form id="movie-search-form" method="get"></Form>
+      <Form id="calendar-manage-form" method="post"></Form>
+      <CalendarModal
+        title="캘린더"
+        onHide={() => console.log("hide")}
+        cancelMessage={"취소"}
+        confirmMessage={"확인"}
+        formId="calendar-manage-form"
+      />
+    </div>
+  );
+};
 
 export default Calendar;
